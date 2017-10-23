8e37ec919c963a8b052c501e10d53f27e291e23e
Michel Hua <michel.hua@mfglabs.com>
2017-10-23 17:49:55 +0200

    Documentation

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ama-bot
-Bot ask for asking me anything
+Bot ask for asking me anything. (Smooch.io + Dialogflow + GitHub Issues + Node.js)
 
 Test the bot here: https://mycaule.github.io/ama-bot/
 
@@ -9,13 +9,33 @@ Proof of concept of a chatbot with integrated backoffices:
  - Dialogflow for user intent configuration,
  - GitHub issues for fallbacks.
 
+## Setup
+
+Fill in the secrets file with API keys `config/secrets.json`.
+
+```
+npm install
+nodemon app.js
+ngrok http 3000
+```
+
+Configure the following two Smooch integration in their web interface:
+- Web messenger
+- Webhooks
+
+## Routes
+
+Available HTTP routes are:
+- `GET /`: A web interface for the chatbot,
+- `POST /`: A webhook implementing Smooch and Dialogflow REST API calls.
+
 ## TODO
 - [x] Implement a webchat bot on Smooch.io
-- [ ] [Dialogflow](https://dialogflow.com) and Smalltalk Intent
+- [x] [Dialogflow](https://dialogflow.com) and Smalltalk Intent
 - [ ] Internet Knowledge for questions off topic
   - Google Answer box
   - [DuckDuckGo Instant Answer API](https://duckduckgo.com/api)
   - [Wolfram API](https://products.wolframalpha.com/api/)
 - [ ] [Google API](https://www.npmjs.com/package/googleapis)
   - Calendar integration
-- [ ] Fill in [Github Issue](https://developer.github.com/v3/issues/) for unanwered questions like in [sindresorhus/ama](https://github.com/sindresorhus/ama)
+- [ ] Fill in [Github Issue](https://developer.github.com/v3/issues/) for unanswered questions like in [sindresorhus/ama](https://github.com/sindresorhus/ama)
--- app.js
+++ app.js
@@ -1,31 +1,69 @@
-let apiai = require('apiai');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
 
 let secrets = require('./config/secrets.json');
-let app = apiai(secrets.dialogflow.clientAccessToken);
 
-const express = require('express');
-const app2 = express();
+const Apiai = require('apiai');
+let dialog = Apiai(secrets.dialogflow.clientAccessToken);
 
-app2.post('/', function (req, res) {
+const Smooch = require('smooch-core');
+const smooch = new Smooch({
+    keyId: secrets.smooch.keyId,
+    secret: secrets.smooch.secretKey,
+    scope: 'app'
+});
+
+// Default GET route for the web messenger client
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use('/coverr', express.static(__dirname + '/coverr'));
+app.use('/smooch', express.static(__dirname + '/smooch'));
+
+// POST webhook route for Smooch
+app.post('/', function (req, res) {
   let json = req.body;
-  let textQuery = json.textQuery;
-  let uniqueSessionId = json.uniqueSessionId;
-  // let textQuery = 'How old are you?';
-  // let uniqueSessionId = '1234';
 
-  let request = app.textRequest(textQuery, { sessionId: uniqueSessionId });
+  let textQuery = json.messages ? json.messages[0].text : "";
+  let appUserId = json.appUser._id;
 
-  request.on('response', function(response) {
-    res.send(response);
+  let request = dialog.textRequest(textQuery, { sessionId: appUserId });
+
+  request.on('response', function(dialogRes) {
+    smooch.appUsers.sendMessage(appUserId, {
+      type: 'text',
+      text: dialogRes.result.fulfillment.speech,
+      role: 'appMaker'
+    })
+    .then((smoochRes) => {
+      res.send({
+        dialogFlow: dialogRes,
+        smooch: smoochRes
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        dialogFlow: dialogRes,
+        smooch: err
+      });
+    });
   });
 
   request.on('error', function(error) {
-    res.status(500).send(error);
+    res.status(500).send({
+      dialogFlow: error,
+      smooch: ""
+    });
   });
 
   request.end();
 });
 
-app2.listen(3000, function () {
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
